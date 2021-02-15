0bd14d4def285dbcbbe90118a872655080b6501d
Luis Guevara <ljguevara2@espe.edu.ec>
2021-02-14 23:20:38 -0500

    Base Project

---
+++ src/main/java/ec/edu/espe/bbconsultas/BuroCreditoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.bbconsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuroCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuroCreditoApplication.class, args);
+	}
+
+}
