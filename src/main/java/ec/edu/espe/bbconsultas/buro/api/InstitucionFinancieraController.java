74c350e712e28dd0d76ca214280e181304f92a01
luisj <ljguevara2@espe.edu.ec>
2021-02-21 18:41:55 -0500

    Add CrossOrigin and change port

--- src/main/java/ec/edu/espe/bbconsultas/buro/api/InstitucionFinancieraController.java
+++ src/main/java/ec/edu/espe/bbconsultas/buro/api/InstitucionFinancieraController.java
@@ -4,6 +4,7 @@ import ec.edu.espe.bbconsultas.buro.exception.DataNotFoundException;
 import ec.edu.espe.bbconsultas.buro.service.InstitucionFinancieraService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author soyjo
  */
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/api/bbConsultas/buro")
 @Slf4j
